9a7daebd77d0159932c15a2ff94e530de7ea76fd
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-06-26 09:00:27 +0530

    Config update with redis URI

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     POSTGRES_URI: process.env.POSTGRES_URL,
+    REDIS_URI: process.env.REDIS_URI,
     GCP_STORAGE_BASE: "https://storage.googleapis.com/terminal-public-buckets/"
 };
