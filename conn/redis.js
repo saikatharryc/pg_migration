f6e38c511833b1f75787cafa1ddf64b4d4e9b133
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-06-26 15:28:05 +0530

    Error Handling on redis connection

--- conn/redis.js
+++ conn/redis.js
@@ -2,4 +2,11 @@ const Redis = require("ioredis");
 const config = require("../config");
 const redis = new Redis(config.REDIS_URI);
 
+redis.on("connected", c => {
+    console.log("Redis connected!");
+});
+redis.on("error", e => {
+    console.log("++++++++++\n" + e + "\n++++++++++");
+    process.exit(0);
+});
 module.exports = { redis };
